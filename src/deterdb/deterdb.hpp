0b7a05949a4564b4596b8b3709d96e9a1c4f1926
Scofield626 <765203006@qq.com>
2023-06-14 12:24:47 +0100

    mod index as shared cown_ptr

--- src/deterdb/deterdb.hpp
+++ src/deterdb/deterdb.hpp
@@ -1,10 +1,16 @@
 #pragma once
 
 #include <unordered_map>
+#include <optional>
 #include <verona.h>
+#include <cpp/when.h>
 
 using namespace verona::rt;
+using namespace verona::cpp;
 
+const uint64_t DB_SIZE = 1000000;
+
+#if 0
 template<typename T>
 struct Row : public VCown<Row<T>>
 {
@@ -38,3 +44,46 @@ public:
     return cnt++;
   }
 };
+#endif
+
+template<typename T>
+struct Row
+{
+  T val;
+
+  ~Row()
+  {
+    printf("Destroying row\n");
+    assert(0);
+  }
+};
+
+template<typename T>
+struct Index
+{
+private:
+  std::shared_ptr<std::array<cown_ptr<Row<T>>, DB_SIZE>> map;
+  uint64_t cnt = 0;
+
+public:
+  cown_ptr<Row<T>> get_row(uint64_t key)
+  {
+    return (*map)[key];
+  }
+
+  uint64_t insert_row(cown_ptr<Row<T>> r)
+  {
+    if (cnt < DB_SIZE)
+    {
+      (*map)[cnt] = r;
+      return cnt++;
+    }
+    else
+    {
+      throw std::out_of_range("Index is full");
+    }
+  }
+};
+
+
+
