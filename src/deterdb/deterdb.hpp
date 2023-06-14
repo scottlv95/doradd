0640114c6a9d54c34415ad4c068e121f27844ad5
Scofield626 <765203006@qq.com>
2023-06-14 17:21:34 +0100

    rm old templates

--- src/deterdb/deterdb.hpp
+++ src/deterdb/deterdb.hpp
@@ -10,42 +10,6 @@ using namespace verona::cpp;
 
 const uint64_t DB_SIZE = 1000000;
 
-#if 0
-template<typename T>
-struct Row : public VCown<Row<T>>
-{
-  T val;
-
-  ~Row()
-  {
-    printf("Destroying row\n");
-    assert(0);
-  }
-};
-
-template<typename T>
-struct Index
-{
-private:
-  //std::unordered_map<uint64_t, Row<T> *> map;
-  Row<T> *map[1000000];
-  uint64_t cnt = 0;
-
-public:
-  Row<T>* get_row(uint64_t key)
-  {
-    return map[key];
-  }
-
-  uint64_t insert_row(Row<T>* r)
-  {
-    map[cnt] = r;
-
-    return cnt++;
-  }
-};
-#endif
-
 template<typename T>
 struct Row
 {
