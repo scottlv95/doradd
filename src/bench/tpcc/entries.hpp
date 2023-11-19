1ae6368df7e6848fbb5c915a2623d6f661e738c2
m8 <umusasadik@gmail.com>
2023-11-19 18:36:47 +0100

    Required headers

--- src/bench/tpcc/entries.hpp
+++ src/bench/tpcc/entries.hpp
@@ -8,24 +8,28 @@
 
 class Warehouse {
    public:
-    uint64_t w_id;
+    uint32_t w_id;
     std::string w_name;
     std::string w_street_1;
     std::string w_street_2;
     std::string w_city;
     std::string w_state;
     std::string w_zip;
-    uint64_t w_tax;
+    uint32_t w_tax;
     uint64_t w_ytd;  // Warehouse balance
 
     // Constructor
-    Warehouse(uint64_t _w_id) : w_id(_w_id) {}
+    Warehouse(uint32_t _w_id) : w_id(_w_id) {}
+
+    uint64_t hash_key() const {
+        return w_id;
+    }
 };
 
 class District {
    public:
-    uint64_t d_id;
-    uint64_t w_id;
+    uint32_t d_id;
+    uint32_t w_id;
     std::string d_name;
     std::string d_street_1;
     std::string d_street_2;
@@ -38,6 +42,10 @@ class District {
 
     // Constructor
     District(uint64_t wid, uint64_t did) : d_id(did), w_id(wid) {}
+
+    uint64_t hash_key() const {
+        return (w_id << 32) | d_id;
+    }
 };
 
 class Item {
@@ -50,6 +58,10 @@ class Item {
 
     // Constructor
     Item(uint64_t _i_id) : i_id(_i_id) {}
+
+    uint64_t hash_key() const {
+        return i_id;
+    }
 };
 
 class Customer {
@@ -77,29 +89,38 @@ class Customer {
     std::string c_data;
 
     Customer(uint64_t _c_w_id, uint64_t _c_d_id, uint64_t _c_id) : c_id(_c_id), c_d_id(_c_d_id), c_w_id(_c_w_id) {}
+
+    uint64_t hash_key() const {
+        return (c_w_id << 32) | (c_d_id << 16) | c_id;
+    }
 };
 
 // Primary Key: (O_W_ID, O_D_ID, O_ID)
 class Order {
    public:
     uint64_t o_id;
-    uint64_t o_d_id;
-    uint64_t o_w_id;
+    uint32_t o_d_id;
+    uint32_t o_w_id;
     uint64_t o_c_id;
     uint64_t o_entry_d;
     uint64_t o_carrier_id;
-    uint64_t o_ol_cnt;
-    uint64_t o_all_local;
+    uint32_t o_ol_cnt;
+    uint32_t o_all_local;
 
-    Order(uint64_t wid, uint64_t did, uint64_t oid) : o_id(oid), o_d_id(did), o_w_id(wid) {}
+    Order(uint32_t wid, uint32_t did, uint64_t oid) : o_id(oid), o_d_id(did), o_w_id(wid) {}
+
+    uint64_t hash_key() const {
+        return (o_w_id << 32) | (o_d_id << 16) | o_id;
+    }
+
 };
 
 // Order line means
 class OrderLine {
    public:
     uint64_t ol_o_id;
-    uint64_t ol_d_id;
-    uint64_t ol_w_id;
+    uint32_t ol_d_id;
+    uint32_t ol_w_id;
     uint64_t ol_i_id;
     uint64_t ol_number;  // Line number
     uint64_t ol_supply_w_id;
@@ -109,8 +130,12 @@ class OrderLine {
     std::string ol_dist_info;
 
     // Constructor
-    OrderLine(uint64_t _w_id, uint64_t _d_id, uint64_t _o_id, uint64_t _number)
+    OrderLine(uint32_t _w_id, uint32_t _d_id, uint64_t _o_id, uint64_t _number)
         : ol_o_id(_o_id), ol_d_id(_d_id), ol_w_id(_w_id), ol_number(_number) {}
+
+    uint64_t hash_key() const {
+        return (ol_w_id << 32) | (ol_d_id << 16) | ol_o_id | ol_number;
+    }
 };
 
 class NewOrder {
@@ -144,6 +169,10 @@ class Stock {
 
     // Constructor
     Stock(uint64_t _w_id, uint64_t _i_id) : s_w_id(_w_id), s_i_id(_i_id) {}
+
+    uint64_t hash_key() const {
+        return (s_w_id << 32) | s_i_id;
+    }
 };
 
 class History {
@@ -158,4 +187,8 @@ class History {
     std::string h_data;
 
     History(uint64_t wid, uint64_t did, uint64_t cid) : h_c_id(cid), h_c_d_id(did), h_c_w_id(wid) {}
+
+    uint64_t hash_key() const {
+        return (h_c_w_id << 32) | (h_c_d_id << 16) | h_c_id;
+    }
 };
