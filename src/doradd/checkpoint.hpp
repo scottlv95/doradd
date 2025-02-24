d0d340c9f5e448bc41c6f066644de8bd2d9f1366
root <root@DESKTOP-LU4VMOS>
2025-02-24 17:15:31 +0000

    WIP: checkpointer

--- src/doradd/checkpoint.hpp
+++ src/doradd/checkpoint.hpp
@@ -9,11 +9,9 @@ public:
   explicit Checkpointer() = default;
 
   // push the current diff set to the checkpoint deque, and clear the current
-  // diff set
-  // must be r value reference
+  // diff set, this will empty the current diff set.
   void push_diff_set(uint64_t transaction_number)
   {
-    std::lock_guard<std::mutex> lock(mtx);
     cown_deque.push_back({transaction_number, std::move(current_diff_set)});
     current_diff_set.clear();
   }
@@ -32,20 +30,17 @@ public:
     return cown_deque.front().first == transaction_number;
   }
 
-  bool popfront_checkpoint(
-    uint64_t& transaction_number, std::set<uint64_t>& diff_set)
+  bool try_checkpoint(uint64_t transaction_number)
   {
-    if (cown_deque.empty())
-      return false;
-    auto pair = std::move(cown_deque.front());
-    cown_deque.pop_front();
-    transaction_number = pair.first;
-    diff_set = std::move(pair.second);
-    return true;
+    if (is_checkpoint_time(transaction_number))
+    {
+      // do checkpointing
+      return true;
+    }
+    return false;
   }
 
 private:
   std::deque<std::pair<uint64_t, std::set<uint64_t>>> cown_deque;
-  mutable std::mutex mtx;
   std::set<uint64_t> current_diff_set;
 };
