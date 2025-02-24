ac7fac7791e4df388f9a86001d8da648af0e64d4
root <root@DESKTOP-LU4VMOS>
2025-02-24 15:44:11 +0000

    Add update diff for checkpointer

--- src/doradd/checkpoint.hpp
+++ src/doradd/checkpoint.hpp
@@ -1,7 +1,6 @@
-
-
 #include <cstdint>
 #include <deque>
+#include <mutex>
 #include <set>
 
 class Checkpointer
@@ -9,29 +8,44 @@ class Checkpointer
 public:
   explicit Checkpointer() = default;
 
-  // only allow move semantics as we don't want to copy the set and
-  // diff set won't be used after this function
-  void save_diff_set(uint64_t transaction_number, std::set<uint64_t>&& diff_set)
+  // push the current diff set to the checkpoint deque, and clear the current
+  // diff set
+  // must be r value reference
+  void push_diff_set(uint64_t transaction_number, std::set<uint64_t>&& diff_set)
   {
-    cown_deque.emplace_back(transaction_number, std::move(diff_set));
+    std::lock_guard<std::mutex> lock(mtx);
+    cown_deque.push_back({transaction_number, std::move(diff_set)});
+    diff_set.clear();
   }
 
-  bool is_checkpoint_time(uint64_t transaction_number) const
+  // update the current diff set
+  void update_diff(uint64_t cown_id)
+  {
+    current_diff_set.insert(cown_id);
+  }
+
+  // check if it is time to create a checkpoint
+  bool is_checkpoint_time(uint64_t transaction_number)
   {
     if (cown_deque.empty())
       return false;
-
     return cown_deque.front().first == transaction_number;
   }
 
-  void checkpoint()
+  bool popfront_checkpoint(
+    uint64_t& transaction_number, std::set<uint64_t>& diff_set)
   {
-    // get the diff set from the front of the deque
-    auto cowns_to_checkpoint = std::move(cown_deque.front().second);
+    if (cown_deque.empty())
+      return false;
+    auto pair = std::move(cown_deque.front());
     cown_deque.pop_front();
+    transaction_number = pair.first;
+    diff_set = std::move(pair.second);
+    return true;
   }
 
 private:
   std::deque<std::pair<uint64_t, std::set<uint64_t>>> cown_deque;
+  mutable std::mutex mtx;
   std::set<uint64_t> current_diff_set;
 };
